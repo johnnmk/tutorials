cbc26f6ea33b1ca3a49732d1906cd56cc6ffb4c8
john <john@10.0.0.128>
2016-07-03 10:17:06 -0700

    added  junit test

---
+++ src/test/java/tutorials/MyTest.java
@@ -0,0 +1,19 @@
+package tutorials;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
